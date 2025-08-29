468d538acbfe21998d79f0cfc689b1e7fc358c78
Nishath Arumugam <nishatharumugam3@gmail.com>
2025-08-29 23:56:44 +0530

    Time: 4 ms (43.71%), Space: 86 MB (5.52%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,16 +1,23 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] a = new int[nums.length];
-        int[]b=new int[nums.length];
-        int acnt =0,bcnt=0;
         //brute force
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>= 0) a[acnt++]=nums[i];
-            else b[bcnt++]=nums[i];
+        int[] a = new int[nums.length];
+        int[] b = new int[nums.length];
+        int acnt=0,bcnt = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] >= 0) a[acnt++] = nums[i];
+            else b[bcnt++] = nums[i];
         }
-        int j=0;int i=0;
-        while(i<acnt && i<bcnt ) {
-            nums[j++]=a[i]; 
-            nums[j++] = b[i]; i++;
-    }return nums;
-}}
+        int i = 0, k = 0;
+        while(i < acnt && i < bcnt){
+            nums[k++] = a[i];
+            nums[k++] = b[i];
+            i++;
+        }
+
+        while(i < acnt) nums[k++] = a[i++];
+        while(i < bcnt) nums[k++] = b[i++];
+
+        return nums;
+    }
+}
