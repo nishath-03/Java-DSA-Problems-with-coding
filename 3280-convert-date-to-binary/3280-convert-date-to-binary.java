123401ada16fea2892a81a8f1861a24f81df8b37
Nishath Arumugam <nishatharumugam3@gmail.com>
2025-09-26 06:47:28 +0530

    Time: 7 ms (43.51%), Space: 42.3 MB (97.33%) - LeetHub

---
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public String convertDateToBinary(String date) {
+        int year = Integer.parseInt(date.substring(0,4));
+        int month = Integer.parseInt(date.substring(5,7));
+        int day = Integer.parseInt(date.substring(8,10));
+        return Integer.toBinaryString(year) + "-" +Integer.toBinaryString(month) + "-" +Integer.toBinaryString(day);
+    }
+}
