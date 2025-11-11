b6f3b9e603dbbd0cb01dfef52d59c13cdaf03b3d
Nishath Arumugam <nishatharumugam3@gmail.com>
2025-11-11 16:38:34 +0530

    Time: 0 ms (100%), Space: 42 MB (10.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,16 @@
-public class Solution {
+class Solution {
     public int climbStairs(int n) {
-        if (n <= 2) return n;
-
-        int first = 1, second = 2, current = 0;
-        for (int i = 3; i <= n; i++) {
-            current = first + second;
-            first = second;
-            second = current;
-        }
-
-        return second;
+        
+        int curr =0,prev1=1,prev2=2;
+        int temp=0;
+      int i=0;
+         while(i!=n){
+            //curr = Math.max(curr,prev1+prev2);
+            curr = prev1+prev2;temp=prev1;
+             
+            prev1=prev2;
+            prev2=curr;
+            i++;
+         }return temp;
     }
 }
